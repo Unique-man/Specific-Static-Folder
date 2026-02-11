30925a1f6fb689382d96d2594c448747a36a5417
Raju Aalakunta <157129711+Unique-man@users.noreply.github.com>
2026-02-11 13:24:42 +0530

    Delete FactApi directory

--- FactApi/app/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-class Fact(models.Model):
-    fact = models.TextField()
-
-    def __str__(self):
-        return self.fact
--- FactApi/app/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from app.models import Fact
-
-class FactMS(serializers.ModelSerializer):
-    class Meta:
-        model = Fact
-        fields = '__all__'
